cb8789d848eb1af4ebf82e1fe4fe6f16663c5344
bharathi2203 <bharathisridhar04@gmail.com>
2025-02-21 05:31:36 +0000

    Deploying to gh-pages from @ bharathi2203/bharathi2203.github.io@57969890c8f40f46097e262e3c284abbc9f78188 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -402,11 +402,6 @@ ninja.data = [{
           description: "with background image",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/4_project/";
             },},{
         id: 'social-email',
         title: 'email',
